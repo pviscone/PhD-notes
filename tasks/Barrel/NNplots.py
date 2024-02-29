2068d8d65eafc76b5e13e028a01050e56f4946f9
pviscone <pieroviscone@gmail.com>
2024-02-29 19:13:02 +0100

    added nn plots

--- tasks/Barrel/NNplots.py
+++ tasks/Barrel/NNplots.py
@@ -4,13 +4,14 @@ from sklearn.metrics import confusion_matrix
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from sklearn.metrics import roc_curve
 import numpy as np
-import matplotlib.ticker as ticker
+import numba as nb
 
 hep.style.use("CMS")
 
-def plot_loss(history,ax=False):
-    if not ax :
-        fig,ax = plt.subplots()
+
+def plot_loss(history, ax=False):
+    if not ax:
+        fig, ax = plt.subplots()
     ax.plot(history.history["loss"], label="loss")
     ax.plot(history.history["val_loss"], label="val_loss")
     ax.grid()
@@ -18,7 +19,7 @@ def plot_loss(history,ax=False):
     return ax
 
 
-def conf_matrix(y_pred, y_test,ax=False):
+def conf_matrix(y_pred, y_test, ax=False):
     if not ax:
         fig, ax = plt.subplots()
     conf = confusion_matrix(y_test.ravel(), y_pred.ravel() > 0.5, normalize="true")
@@ -37,14 +38,14 @@ def conf_matrix(y_pred, y_test,ax=False):
 
 
 # * 11245.6 * 2500 / 1e3
-def roc_plot(y_pred, y_test,xlim=0.1,ax=False):
+def roc_plot(y_pred, y_test, xlim=0.1, ax=False):
     if not ax:
         fig, ax = plt.subplots()
     fpr, tpr, tr = roc_curve(y_test.ravel(), y_pred.ravel())
-    tr=np.arctanh(tr)
+    tr = np.arctanh(tr)
 
     # plot the eddiciecy vs trigger rate for phase 2
-    mappable=ax.scatter(fpr , tpr, c=tr, marker=".")
+    mappable = ax.scatter(fpr, tpr, c=tr, marker=".")
     ax.set_xlabel("Bkg efficiency")
     ax.set_ylabel("Electron efficiency")
     ax.grid()
@@ -52,21 +53,38 @@ def roc_plot(y_pred, y_test,xlim=0.1,ax=False):
 
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(mappable,ax=ax, cax=cax, label="Threshold", orientation="vertical")
+    plt.colorbar(mappable, ax=ax, cax=cax, label="Threshold", orientation="vertical")
 
     return ax
 
-def out_plot(y_pred, y_test, bins=30, significance=False,ax=False):
+
+def out_plot(y_pred, y_test, bins=30, significance=False, ax=False):
     if not ax:
         fig, ax = plt.subplots()
-    hs = ax.hist(y_pred[y_test == 1], bins=bins, histtype="step", density=False,label="Signal",linewidth=2)
-    hb = ax.hist(y_pred[y_test == 0], bins=bins, histtype="step", density=False,label="Background",linewidth=2)
+    hs = ax.hist(
+        y_pred[y_test == 1],
+        bins=bins,
+        histtype="step",
+        density=False,
+        label="Signal",
+        linewidth=2,
+    )
+    hb = ax.hist(
+        y_pred[y_test == 0],
+        bins=bins,
+        histtype="step",
+        density=False,
+        label="Background",
+        linewidth=2,
+    )
 
     ax.set_yscale("log")
 
     if significance:
         centers = (hs[1][:-1] + hs[1][1:]) / 2
-        ax.step(centers, hs[0]/ np.sqrt(hs[0] + hb[0]), label="S/$\sqrt{S+B}$", where="mid")
+        ax.step(
+            centers, hs[0] / np.sqrt(hs[0] + hb[0]), label="S/$\sqrt{S+B}$", where="mid"
+        )
 
     ax.set_xlabel("atanh(NNScore)")
     ax.grid()
@@ -75,61 +93,73 @@ def out_plot(y_pred, y_test, bins=30, significance=False,ax=False):
     return ax
 
 
-def efficiency_plot(y_pred,
-                    y_test,
-                    genpt,
-                    threshold=0.5,
-                    label=None,
-                    ax=False):
+def efficiency_plot(y_pred, y_test, genpt, threshold=0.5, label=None, ax=False):
     if not ax:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
 
     genHist = np.histogram(genpt[y_test == 1], bins=50, range=(0, 100))
 
-
     genPredHist = np.histogram(
-        genpt[np.bitwise_and(y_test == 1,
-                            (y_pred > threshold).
-                            astype(int) == y_test)],
+        genpt[np.bitwise_and(y_test == 1, (y_pred > threshold).astype(int) == y_test)],
         bins=50,
         range=(0, 100),
     )
 
     centers = (genHist[1][:-1] + genHist[1][1:]) / 2
-    ax.step(centers, genPredHist[0] / genHist[0], where="mid", label=label,linewidth=2)
+    ax.step(
+        centers,
+        genPredHist[0] / genHist[0],
+        where="mid",
+        label=f"Threshold: {np.arctanh(threshold):.2f}",
+        linewidth=2,
+    )
     ax.set_xlabel("genPt [GeV]")
     ax.set_ylabel("Efficiency")
     ax.grid()
     return ax
 
 
+def loop_on_trs(func, *args, ax=False, trs=np.linspace(0.1, 3, 6), **kwargs):
+    if not ax:
+        _, ax = plt.subplots()
+    for tr in np.tanh(trs):
+        func(*args, threshold=tr, ax=ax, **kwargs)
+    ax.legend()
+    ax.set_ylim(-0.1, 1.1)
+    ax.grid()
+    return ax
+
+
+"""
 def tr_binned_eff(y_pred,
                   y_test,
                   genpt,
                   trs=np.linspace(0.1,3,6),ax=False):
     if not ax:
-        #fig, ax = plt.subplots(len(trs),1,sharex=True,figsize=(10,30))
-        fig,ax = plt.subplots()
-    #plt.subplots_adjust(hspace=0)
+        _,ax = plt.subplots()
 
-    for idx,tr in enumerate(trs):
+
+    for tr in trs:
 
         efficiency_plot(y_pred,
                         y_test,
                         genpt,
                         threshold=np.tanh(tr),
                         ax=ax,
-                        #ax=ax[idx],
                         label=f"Threshold: {tr:.2f}")
     ax.legend()
     ax.set_ylim(-0.1,1.1)
     ax.grid()
-        #ax[idx].legend()
-        #ax[idx].set_ylim(-0.1,1.1)
     return ax
+"""
 
 
-def trigger_rate(y_pred,y_test,ev_idx,ax=False):
-    if not ax:
-        fig, ax = plt.subplots()
-    pass
+
+
+
+
+
+
+
+
+
